c6f1716b0f368a0f56e883d6899652e32394d33d
jhamilgarcia <jhamilgar@gmail.com>
2025-09-10 23:17:04 -0400

    creacion del toolBar en androidStudio

--- app/src/main/java/com/example/infogram/createAccountActivity.java
+++ app/src/main/java/com/example/infogram/createAccountActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +16,15 @@ public class createAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getString(R.string.title_crear_cuenta), true);
+    }
+
+    public void showToolbar(String titulo, boolean botonSubir) {
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
 
     }
 }
